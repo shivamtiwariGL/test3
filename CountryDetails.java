a7b3f12ff8109b9c259be58c281d76cd357da7b1
shivamtiwariGL <110605224+shivamtiwariGL@users.noreply.github.com>
2022-08-08 16:19:23 +0530

    Add files via upload

---
+++ CountryDetails.java
@@ -0,0 +1,37 @@
+/*Develop a java class with a instance variable M1 (HashMap) create a method saveCountryCapital(String CountryName, String capital) , the method should add the passed country and capital as key/value in the map M1 and return the Map (M1).
+Key- Country Value - Capital
+India Delhi
+Japan Tokyo
+2. Develop a method getCapital(String CountryName) which returns the capital for the country passed, from the Map M1 created in step 1.
+3. Develop a method getCountry(String capitalName) which returns the country for the capital name, passed from the Map M1 created in step 1.
+4. Develop a method which iterates through the map M1 and creates another map M2 with Capital as the key and value as Country and returns the Map M2.
+Key – Capital Value – Country
+Delhi India
+Tokyo Japan
+5. Develop a method which iterates through the map M1 and creates an ArrayList with all the Country names stored as keys. This method should return the ArrayList.
+NOTE: You can test the methods using a main method. */
+
+
+
+
+package com.Assessment;
+
+import java.util.HashMap;
+
+public class CountryDetails 
+{
+    public static void main( String[] args ) {
+    	Country country = new Country();
+    	country.saveCountryCapital("India ", " New Delhi");
+    	country.saveCountryCapital("Japan ", " Tokyo");
+    
+    	
+    	System.out.println(country.getCapital("India"));
+    	System.out.println(country.getCountry("Tokyo"));
+    	System.out.println(country.toArrayList());
+    	
+    	
+    	HashMap<String, String> M2 = country.swapKeyValue();
+    	System.out.println(M2);
+    }
+}
